7ebbc6238391ff58dd3b948ddd16c1b89ae4b85e
ISHII Tsuubito <ishii@buffer-renaiss.com>
2014-05-08 22:33:28 +0900

    initial

---
+++ File Templates/Other/GPUImageClass.xctemplate/GPUImageFilterCustomFIlter/___FILEBASENAME___.m
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)init {
+    self = [super initWithFragmentShaderFromFile:@"___FILEBASENAME___"];
+    if(!self) {
+        return nil;
+    }
+    
+    return self;
+}
+
+@end
